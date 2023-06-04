ddd077952b2fc764efce2dfbaca679c8cbb4d096
Hugo Ratts <118541950+vratts@users.noreply.github.com>
2023-06-04 19:25:38 -0300

    Create print code #3

--- pages/api/v1/screenshot.js
+++ pages/api/v1/screenshot.js
@@ -1,28 +1,102 @@
-import chrome from 'chrome-aws-lambda';
-import { join } from 'path';
-import { createReadStream } from 'fs';
+// Shout outs to the following repositories:
 
-export default async (req, res) => {
-    const { url } = req.query;
+// https://github.com/vercel/og-image
+// https://github.com/ireade/netlify-puppeteer-screenshot-demo
 
-    try {
-        const browser = await chrome.puppeteer.launch({
+// The maximum execution timeout is 10
+// seconds when deployed on a Personal Account (Hobby plan).
+// For Teams, the execution timeout is 60 seconds (Pro plan)
+// or 900 seconds (Enterprise plan).
+
+const puppeteer = require("puppeteer-core");
+const chrome = require("chrome-aws-lambda");
+
+/** The code below determines the executable location for Chrome to
+ * start up and take the screenshot when running a local development environment.
+ *
+ * If the code is running on Windows, find chrome.exe in the default location.
+ * If the code is running on Linux, find the Chrome installation in the default location.
+ * If the code is running on MacOS, find the Chrome installation in the default location.
+ * You may need to update this code when running it locally depending on the location of
+ * your Chrome installation on your operating system.
+ */
+
+const exePath =
+    process.platform === "win32"
+        ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+        : process.platform === "linux"
+            ? "/usr/bin/google-chrome"
+            : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+
+async function getOptions(isDev) {
+    let options;
+    if (isDev) {
+        options = {
+            args: [],
+            executablePath: exePath,
+            headless: true,
+        };
+    } else {
+        options = {
             args: chrome.args,
             executablePath: await chrome.executablePath,
             headless: chrome.headless,
-        });
+        };
+    }
+    return options;
+}
+
+module.exports = async (req, res) => {
+    const pageToScreenshot = req.query.page;
+
+    // pass in this parameter if you are developing locally
+    // to ensure puppeteer picks up your machine installation of
+    // Chrome via the configurable options
+    const isDev = req.query.isDev === "true";
+
+    try {
+        // check for https for safety!
+        if (!pageToScreenshot.includes("https://")) {
+            res.statusCode = 404;
+            res.json({
+                body: "Sorry, we couldn't screenshot that page. Did you include https://?",
+            });
+        }
+
+        // get options for browser
+        const options = await getOptions(isDev);
+
+        // launch a new headless browser with dev / prod options
+        const browser = await puppeteer.launch(options);
         const page = await browser.newPage();
-        await page.goto(url, { waitUntil: 'networkidle0' });
-        const screenshotPath = join(process.cwd(), 'public', 'screenshot.png');
-        await page.screenshot({ path: screenshotPath, fullPage: true });
+
+        // set the viewport size
+        await page.setViewport({
+            width: 1920,
+            height: 1080,
+            deviceScaleFactor: 1,
+        });
+
+        // tell the page to visit the url
+        await page.goto(pageToScreenshot);
+
+        // take a screenshot
+        const file = await page.screenshot({
+            type: "png",
+        });
+
+        // close the browser
         await browser.close();
 
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'image/png');
-        createReadStream(screenshotPath).pipe(res);
-    } catch (error) {
-        console.error(error);
+        res.setHeader("Content-Type", `image/png`);
+
+        // return the file!
+        res.end(file);
+    } catch (e) {
         res.statusCode = 500;
-        res.end('Internal Server Error');
+        res.json({
+            body: "Sorry, Something went wrong!",
+        });
     }
 };
