cab5b2bcb31bca68f71e42d958505eb1be751784
Hugo Ratts <118541950+vratts@users.noreply.github.com>
2023-10-16 17:42:28 -0300

    Create HTML-Converter to Image #3

--- pages/api/v1/html-render.js
+++ pages/api/v1/html-render.js
@@ -1,17 +1,22 @@
-import htmlToImage from "html-to-image";
+// import * as htmlToImage from 'html-to-image';
+import { createCanvas } from 'canvas';
+import htmlToImage from 'node-html-to-image';
+import fs from 'fs';
 
-export default async function (req, res){
-    res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate=59");
+export default async function (req, res) {
     if (req.method !== 'POST') {
         res.status(405).json({ message: 'Only POST requests allowed' })
         return;
     }
 
     var body = req.body;
-    console.log(body)
-    const imagemBase64 = await htmlToImage.toPng(body.html);
-    res.json({
-        status: true,
-        content: imagemBase64
+    await htmlToImage({
+        output: '/tmp/upload',
+        html: body.html
     });
+
+    const image = fs.readFileSync('/tmp/upload');
+    const base64Image = new Buffer.from(image).toString('base64');
+    const dataURI = 'data:image/jpeg;base64,' + base64Image;
+    res.json({ status: true, dataURI })
 }
