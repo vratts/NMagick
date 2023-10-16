0f265d260bbb9f8544947cfe945d0fe53c3432a7
Hugo Ratts <118541950+vratts@users.noreply.github.com>
2023-10-16 15:07:00 -0300

    Create HTML-Converter to Image

---
+++ pages/api/v1/html-render.js
@@ -0,0 +1,17 @@
+import htmlToImage from "html-to-image";
+
+export default async function HtmlRender(req, res){
+    res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate=59");
+    if (req.method !== 'POST') {
+        res.status(405).json({ message: 'Only POST requests allowed' })
+        return;
+    }
+
+    var body = req.body;
+
+    const imagemBase64 = await htmlToImage.toPng(body.html);
+    res.json({
+        status: true,
+        content: imagemBase64
+    });
+}
